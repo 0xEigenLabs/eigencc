484fc1b87205318bca2944f3463b83c5afd1b602
eigmax <stephen@ieigen.com>
2021-07-19 00:02:52 +0800

    Add ecies, interact with rust ECIES

--- l2/pkcs/src/ecies.js
+++ l2/pkcs/src/ecies.js
@@ -3,32 +3,32 @@
 
 const crypto = require('crypto');
 const assert = require('assert');
+var EC = require("elliptic").ec;
+var ec = new EC("p256");
 const empty_buffer = Buffer.allocUnsafe ? Buffer.allocUnsafe(0) : Buffer.from([]);
-const SALT_LEN = 32;
+const NONCE_LEN = 32;
 const AUTH_TAG_LEN = 16;
 const IV_LEN = 12;
 
 // E
 function symmetricEncrypt(cypherName, iv, key, plaintext) {
     let cipher = crypto.createCipheriv(cypherName, key, iv);
-    var encrypted = Buffer.concat([cipher.update(plaintext, 'utf8'), cipher.final()]);
-
+    var encrypted = Buffer.concat([cipher.update(plaintext), cipher.final()]);
     let tag  = cipher.getAuthTag()
-    var salt = crypto.randomBytes(SALT_LEN);
-    return Buffer.concat([salt, iv, tag, encrypted]).toString('base64');
+    var nonce = crypto.randomBytes(NONCE_LEN);
+    return Buffer.concat([nonce, iv, tag, encrypted]);
 }
 
 // E-1
-function symmetricDecrypt(cypherName, key, ciphertext) {
-    var bData = Buffer.from(ciphertext, 'base64');
+function symmetricDecrypt(cypherName, key, cipherText) {
     // convert data to buffers
-    let salt = bData.slice(0, SALT_LEN);
-    let iv = bData.slice(SALT_LEN, SALT_LEN + IV_LEN);
-    let tag = bData.slice(SALT_LEN + IV_LEN, SALT_LEN + IV_LEN + AUTH_TAG_LEN);
-    let text = bData.slice(SALT_LEN + IV_LEN + AUTH_TAG_LEN);
-    let cipher = crypto.createDecipheriv(cypherName, key, iv);
-    cipher.setAuthTag(tag);
-    let dec = cipher.update(text, 'binary', 'utf8') + cipher.final('utf8');
+    let nonce = cipherText.slice(0, NONCE_LEN);
+    let iv = cipherText.slice(NONCE_LEN, NONCE_LEN + IV_LEN);
+    let tag = cipherText.slice(NONCE_LEN + IV_LEN, NONCE_LEN + IV_LEN + AUTH_TAG_LEN);
+    let text = cipherText.slice(NONCE_LEN + IV_LEN + AUTH_TAG_LEN);
+    let decipher = crypto.createDecipheriv(cypherName, key, iv);
+    decipher.setAuthTag(tag);
+    let dec = decipher.update(text) + decipher.final();
     return dec;
 }
 exports.aes_enc = symmetricEncrypt
@@ -91,11 +91,13 @@ function makeUpOptions(options) {
 exports.encrypt = function (publicKey, message, options) {
     options = makeUpOptions(options);
 
-    const ecdh = crypto.createECDH(options.curveName);
+    const ephemPrivateKey = ec.genKeyPair();
+    console.log(ephemPrivateKey);
     // R
-    const R = ecdh.generateKeys(null, options.keyFormat);
+    const ephemPublicKey = Buffer.from(ephemPrivateKey.getPublic("arr"));
     // S
-    const sharedSecret = ecdh.computeSecret(publicKey);
+    const sharedSecret = Buffer.from(ephemPrivateKey.derive(publicKey).toArray());
+    console.log("shared secret", sharedSecret)
 
     // uses KDF to derive a symmetric encryption and a MAC keys:
     // Ke || Km = KDF(S || S1)
@@ -127,22 +129,29 @@ exports.encrypt = function (publicKey, message, options) {
         ),
     );
     // outputs R || c || d
-    return Buffer.concat([R, bufCipherText, tag]);
+    console.log("ephem public key", ephemPublicKey);
+    return Buffer.concat([ephemPublicKey, bufCipherText, tag]);
 };
 
-exports.decrypt = function (ecdh, message, options) {
+exports.decrypt = function (keyPair, message, options) {
     options = makeUpOptions(options);
 
-    const publicKeyLength = ecdh.getPublicKey(null, options.keyFormat).length;
+    const publicKeyLength = keyPair.getPublic("arr").length;
+    console.log("length ", publicKeyLength);
     // R
     const R = message.slice(0, publicKeyLength);
+    console.log(R);
+    let keyPair2 = ec.keyFromPublic(R);
+    let publicKey = keyPair2.getPublic();
     // c
     const cipherText = message.slice(publicKeyLength, message.length - options.macLength);
     // d
     const messageTag = message.slice(message.length - options.macLength);
 
     // S
-    const sharedSecret = ecdh.computeSecret(R);
+    const sharedSecret = Buffer.from(keyPair.derive(publicKey).toArray());
+    console.log("p.type: ", publicKey.type);
+    console.log("encryption shared secret", sharedSecret)
 
     // derives keys the same way as Alice did:
     // Ke || Km = KDF(S || S1)
@@ -167,6 +176,8 @@ exports.decrypt = function (ecdh, message, options) {
             cipherText.length + options.s2.length
         )
     );
+    console.log("messageTag", "keyTag", "macKey", "aes key", "cipher");
+    console.log(messageTag, keyTag, macKey, cipherText);
 
     // outputs failed if d != MAC(Km; c || S2);
     assert(equalConstTime(messageTag, keyTag), "Bad MAC");
