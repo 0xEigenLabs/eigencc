8c74d8a5d1bd369f0b592972e95395e80a09aa08
Weber❤️H2O2 <82763013+Weber-H2O2@users.noreply.github.com>
2021-09-16 12:23:54 +0800

    eigen_service supports more features!  😋  😋  😋  (#111)
    * A draft implementation for eigen_service
    
    * Use 'findAndCountAll' is better way to implement page support
    
    * Use 'updatedAt' instead of 'createdAt' to order the transactions
    
    * L1->L2 transactions should not be included when searching on L2
    
    * Support filter 'from' or 'to' value when searching on L2
    
    * Transaction or account count should not include L1->L2 transactions
    
    * Fix get the count of accounts on L2
    
    * Update document

--- l2/eigen_service/README.md
+++ l2/eigen_service/README.md
@@ -1,18 +1,22 @@
 # Eigen Service
-* PKCS
-* Transaction History
+
+- PKCS
+- Transaction History
 
 ## Test
+
 `yarn test`
 
 ## Usage
 
 ### Launch Server
+
 ```
 forever start ./src/app.js  # or `yarn start` for dev
 ```
 
 ### PKCS
+
 Simple local public key cache service on sqlite, with ecies inside.
 
 ```
@@ -26,11 +30,13 @@ curl -XGET -H "Content-Type:application/json"  --url "localhost:3000/stores"
 # add
 curl -XPOST -H "Content-Type:application/json"  --url "localhost:3000/store" -d '{"digest":"1", "public_key":"pk"}'
 ```
+
 ### Transaction History
 
-#### Data type 
-* status： status of the transaction, 0: new, 1: confirmed, 2.  only for withdraw, confirmed in Layer 1
-* type: transaction type
+#### Data type
+
+- status： status of the transaction, 0: new, 1: confirmed, 2. only for withdraw, confirmed in Layer 1
+- type: transaction type
 
 ```
 const TX_TYPE_L1ToL1 = 0x0
@@ -44,23 +50,42 @@ const TX_TYPE_L2ToL2 = 0x3
 ```
 #query
 curl -XGET -H "Content-Type:application/json"  --url "localhost:3000/txh?txid=1"
-#search all
+
+#search all (with/without filters)
 curl -XGET -H "Content-Type:application/json"  --url "localhost:3000/txhs?action=search&from=0x1"
+
+#query all transactions with the reverse time order (also support page)
+curl -XGET -H "Content-Type:application/json"  --url "localhost:3000/txhs?action=search&order=1"
+
+#query all transactions by page number
+curl -XGET -H "Content-Type:application/json"  --url "localhost:3000/txhs?action=search&page=1&page_size=10"
+
+#query the count of all transactions on L2 (L2 -> L1, L2 -> L2)
+curl -XGET -H "Content-Type:application/json"  --url "localhost:3000/txh?action=transaction_count_l2"
+
+#query the count of all accounts on L2 ('from' on L2 -> L1, 'from' and 'to' on L2 -> L2)
+curl -XGET -H "Content-Type:application/json"  --url "localhost:3000/txh?action=account_count_l2"
+
 #add
-curl -XPOST -H "Content-Type:application/json"  --url "localhost:3000/txh" -d '{"txid": "1", "from": "0x1", "to": "0x1", "type":0, "value": 1}'
+curl -XPOST -H "Content-Type:application/json"  --url "localhost:3000/txh" -d '{"txid": "1", "from": "0x1", "to": "0x1", "type":0, "value": 1, "block_num": 1027}'
+
 #update
 curl -XPUT -H "Content-Type:application/json"  --url "localhost:3000/txh/{txid}" -d '{"status": 1, "sub_txid": "2121"}'
+
+#query all transactions on L2 (L1 -> L2 and L2 -> L1, with/witout filters, also support page and reverse order)
+curl -XGET -H "Content-Type:application/json"  --url "localhost:3000/txhs?action=search_l2&from=0x1&page=1&page_size=10&order=1"
 ```
 
 ## Deployment in production
 
 ### Build
+
 ```
 docker build -t ieigen/service:v1 .
 ```
 
 ### Run
+
 ```
 docker run --name=eigen-service -p 3000:3000 -d ieigen/service:v1
 ```
-
--- l2/eigen_service/src/app.js
+++ l2/eigen_service/src/app.js
@@ -1,59 +1,62 @@
-const express = require('express')
+const express = require("express");
 const log4js = require("./log");
-var logger = log4js.logger("APP")
-var db_pk = require("./database_pk")
-var db_txh = require("./database_transaction_history")
-var util = require("./util.js")
+var logger = log4js.logger("APP");
+var db_pk = require("./database_pk");
+var db_txh = require("./database_transaction_history");
+var util = require("./util.js");
+const { Op } = require("sequelize");
 
-const app = express()
+const app = express();
 app.use(log4js.useLog());
 
-
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 // query key
-app.get('/stores', async function (req, res) {
-    return res.json(util.Succ(await db_pk.findAll()))
-})
+app.get("/stores", async function (req, res) {
+  return res.json(util.Succ(await db_pk.findAll()));
+});
 
-app.get('/store', async function (req, res) {
-    var digest = req.query.digest
-    if (!util.has_value(digest)) {
-        logger.error("digest is empty")
-        return res.json(util.Err(1, "digest missing"))
-    }
-    var result = await db_pk.findByDigest(digest);
-    if (!result) {
-        return res.json(util.Succ({}))
-    }
-    res.json(util.Succ(result))
-})
+app.get("/store", async function (req, res) {
+  var digest = req.query.digest;
+  if (!util.has_value(digest)) {
+    logger.error("digest is empty");
+    return res.json(util.Err(1, "digest missing"));
+  }
+  var result = await db_pk.findByDigest(digest);
+  if (!result) {
+    return res.json(util.Succ({}));
+  }
+  res.json(util.Succ(result));
+});
 
 // add new key
 app.post("/store", async function (req, res) {
-    var digest = req.body.digest;
-    var pk = req.body.public_key;
-    if (!util.has_value(digest) || !util.has_value(pk)) {
-        return res.json(util.Err(1, "missing dig or pk"))
-    }
+  var digest = req.body.digest;
+  var pk = req.body.public_key;
+  if (!util.has_value(digest) || !util.has_value(pk)) {
+    return res.json(util.Err(1, "missing dig or pk"));
+  }
 
-    var result = db_pk.updateOrAdd(digest, digest, pk);
-    res.json(util.Succ(result))
-})
+  var result = db_pk.updateOrAdd(digest, digest, pk);
+  res.json(util.Succ(result));
+});
 
 // update
 app.put("/store", async function (req, res) {
-    var old_digest = req.body.old_digest;
-    var digest = req.body.digest;
-    var pk = req.body.public_key;
-    if (!util.has_value(digest) || !util.has_value(pk) ||
-        !util.has_value(old_digest)) {
-        return res.json(util.Err(1, "missing dig or pk"))
-    }
-    var result = db_pk.updateOrAdd(old_digest, digest, pk);
-    res.json(util.Succ(result))
-})
+  var old_digest = req.body.old_digest;
+  var digest = req.body.digest;
+  var pk = req.body.public_key;
+  if (
+    !util.has_value(digest) ||
+    !util.has_value(pk) ||
+    !util.has_value(old_digest)
+  ) {
+    return res.json(util.Err(1, "missing dig or pk"));
+  }
+  var result = db_pk.updateOrAdd(old_digest, digest, pk);
+  res.json(util.Succ(result));
+});
 
 /*
 app.get("/txhs", async function(req, res) {
@@ -61,72 +64,119 @@ app.get("/txhs", async function(req, res) {
 })
 */
 
-//TODO: retrieve by page
-app.get("/txhs", async function(req, res) {
-    var action = req.query.action
-    console.log(req.query)
-    if (action != "search") {
-        return res.json(util.Err(1, "invalid action"))
-    }
-    let dict = req.query
-    delete dict['action']
-    return res.json(util.Succ(await db_txh.search(req.query)))
-})
+app.get("/txhs", async function (req, res) {
+  var action = req.query.action;
+  console.log(req.query);
+  let dict = req.query;
+  let page = dict["page"];
+  let page_size = dict["page_size"];
+  let order = dict["order"];
+  switch (action) {
+    case "search":
+      delete dict["action"];
+      delete dict["page"];
+      delete dict["page_size"];
+      delete dict["order"];
+      return res.json(
+        util.Succ(await db_txh.search(req.query, page, page_size, order))
+      );
+      break;
+    case "search_l2":
+      delete dict["action"];
+      delete dict["page"];
+      delete dict["page_size"];
+      delete dict["order"];
 
-app.get("/txh", async function(req, res) {
-    var txid = req.query.txid
+      // TODO: 0x2 (L2->L1), 0x3 (L2->L2) should replaced with enum
+      dict["type"] = {
+        [Op.or]: [0x2, 0x3],
+      };
+      return res.json(
+        util.Succ(await db_txh.search(req.query, page, page_size, order))
+      );
+      break;
+
+    default:
+      return res.json(util.Err(1, "invalid action"));
+      break;
+  }
+});
+
+app.get("/txh", async function (req, res) {
+  var action = req.query.action;
+  console.log("action = ", action);
+  if (!action) {
+    var txid = req.query.txid;
     if (!util.has_value(txid)) {
-        logger.error("txid is empty")
-        return res.json(util.Err(1, "txid missing"))
+      logger.error("txid is empty");
+      return res.json(util.Err(1, "txid missing"));
     }
     var result = await db_txh.getByTxid(txid);
     if (!result) {
-        return res.json(util.Succ({}))
+      return res.json(util.Succ({}));
     }
-    res.json(util.Succ(result))
-})
+    res.json(util.Succ(result));
+  } else {
+    switch (action) {
+      case "transaction_count_l2":
+        return res.json(util.Succ(await db_txh.transaction_count_l2()));
+        break;
+      case "account_count_l2":
+        return res.json(util.Succ(await db_txh.account_count_l2()));
+        break;
+      default:
+        return res.json(util.Err(1, "invalid action"));
+        break;
+    }
+  }
+});
 
-// add new key
+// add transaction
 app.post("/txh", async function (req, res) {
-    var txid = req.body.txid;
-    var from = req.body.from;
-    var to = req.body.to;
-    var value = req.body.value;
-    var type = req.body.type;
-    if (!util.has_value(txid) ||
-        !util.has_value(from) ||
-        !util.has_value(value) ||
-        !util.has_value(to) ||
-        !util.has_value(type)) {
-        return res.json(util.Err(1, "missing fields"))
-    }
-    console.log(req.body);
+  var txid = req.body.txid;
+  var from = req.body.from;
+  var to = req.body.to;
+  var value = req.body.value;
+  var block_num = req.body.block_num;
+  var type = req.body.type;
+  if (
+    !util.has_value(txid) ||
+    !util.has_value(from) ||
+    !util.has_value(value) ||
+    !util.has_value(to) ||
+    !util.has_value(block_num) ||
+    !util.has_value(type)
+  ) {
+    return res.json(util.Err(1, "missing fields"));
+  }
+  console.log(req.body);
 
-    var result = db_txh.updateOrAdd(txid, {
-        "txid": txid,
-        "from": from,
-        "to": to,
-        "value": value,
-        "type": Number(type),
-        "status": req.body.status || 0,
-        "sub_txid": req.body.sub_txid || ''
-    });
-    res.json(util.Succ(result))
-})
+  var result = db_txh.updateOrAdd(txid, {
+    txid: txid,
+    from: from,
+    to: to,
+    value: value,
+    type: Number(type),
+    block_num: block_num,
+    status: req.body.status || 0,
+    sub_txid: req.body.sub_txid || "",
+  });
+  res.json(util.Succ(result));
+});
 
-// update
+// update transaction status
 app.put("/txh/:txid", async function (req, res) {
-    var txid = req.params.txid;
-    if (!util.has_value(txid)) {
-        return res.json(util.Err(1, "missing fields"))
-    }
-    var result = db_txh.updateOrAdd(txid, {
-        "status": req.body.status || 0,
-        "sub_txid": req.body.sub_txid || ''
-    });
-    res.json(util.Succ(result))
-})
+  var txid = req.params.txid;
+  if (!util.has_value(txid)) {
+    return res.json(util.Err(1, "missing fields"));
+  }
+  var result = db_txh.updateOrAdd(txid, {
+    status: req.body.status || 0,
+    sub_txid: req.body.sub_txid || "",
+  });
+  res.json(util.Succ(result));
+});
 
-app.listen(3000, function() {
-    console.log("Eigen Service listening on port 3000!");
-})
+app.listen(3000, function () {
+  console.log("Eigen Service listening on port 3000!");
+});
