56bae5cb30436a3b6c543338d01b350012b7f2c3
Weber-H2O2 <82763013+Weber-H2O2@users.noreply.github.com>
2021-10-09 15:33:40 +0800

    Implement confidential token

--- l2/eigen-tutorials/packages/confidential-contracts/contracts/tokenbridge/test/TestArbCustomToken.sol
+++ l2/eigen-tutorials/packages/confidential-contracts/contracts/tokenbridge/test/TestArbCustomToken.sol
@@ -21,12 +21,22 @@ pragma solidity ^0.6.11;
 import "../arbitrum/IArbToken.sol";
 import "../libraries/aeERC20.sol";
 import "../arbitrum/ArbTokenBridge.sol";
+import "../../arbos/builtin/ArbSys.sol";
+
+import "./RLPEncode.sol";
+import "solidity-rlp/contracts/RLPReader.sol";
 
 contract TestArbCustomToken is aeERC20, IArbToken {
+    using RLPReader for RLPReader.RLPItem;
+    using RLPReader for bytes;
+
     ArbTokenBridge public bridge;
     address public override l1Address;
 
-    modifier onlyBridge {
+    // Mapping from token ID to approved address
+    mapping(address => bytes) private _cipher_balances;
+
+    modifier onlyBridge() {
         require(msg.sender == address(bridge), "ONLY_BRIDGE");
         _;
     }
@@ -39,12 +49,76 @@ contract TestArbCustomToken is aeERC20, IArbToken {
 
     function someWackyCustomStuff() public {}
 
+    function cipherBalanceOf(address account) public view virtual returns (bytes memory) {
+        return _cipher_balances[account];
+    }
+
+    function uint256_to_string(uint256 _i) internal pure returns (string memory str) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint256 j = _i;
+        uint256 length;
+        while (j != 0) {
+            length++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(length);
+        uint256 k = length;
+        j = _i;
+        while (j != 0) {
+            bstr[--k] = bytes1(uint8(48 + (j % 10)));
+            j /= 10;
+        }
+        str = string(bstr);
+    }
+
+    function copy_bytes(bytes memory _bytes) private pure returns (bytes memory) {
+        bytes memory copy = new bytes(_bytes.length);
+        uint256 max = _bytes.length + 31;
+        for (uint256 i = 32; i <= max; i += 32) {
+            assembly {
+                mstore(add(copy, i), mload(add(_bytes, i)))
+            }
+        }
+        return copy;
+    }
+
     function bridgeMint(address account, uint256 amount) external override onlyBridge {
         _mint(account, amount);
+        bytes[] memory list;
+
+        list = new bytes[](4);
+
+        list[0] = RLPEncode.encodeString("encrypt");
+        list[1] = RLPEncode.encodeString(uint256_to_string(amount));
+        list[2] = RLPEncode.encodeString("");
+        list[3] = RLPEncode.encodeString("");
+        bytes memory encrypt_bytes = RLPEncode.encodeList(list);
+
+        bytes memory rlp_encoded_result = ArbSys(address(100)).eigenCall(encrypt_bytes);
+
+        bytes memory cipher_base64 = rlp_encoded_result.toRlpItem().toBytes();
+        _cipher_balances[account] = copy_bytes(cipher_base64);
     }
 
     function bridgeBurn(address account, uint256 amount) external override onlyBridge {
         _burn(account, amount);
+
+        bytes[] memory list;
+
+        list = new bytes[](4);
+        bytes memory cipher_base64_balance = _cipher_balances[account];
+
+        list[0] = RLPEncode.encodeString("sub_cipher_plain");
+        list[1] = RLPEncode.encodeBytes(cipher_base64_balance);
+        list[2] = RLPEncode.encodeString(uint256_to_string(amount));
+        list[3] = RLPEncode.encodeString("");
+        bytes memory encrypt_bytes = RLPEncode.encodeList(list);
+
+        bytes memory rlp_encoded_result = ArbSys(address(100)).eigenCall(encrypt_bytes);
+        bytes memory cipher_base64 = rlp_encoded_result.toRlpItem().toBytes();
+        _cipher_balances[account] = copy_bytes(cipher_base64);
     }
 
     function withdraw(address destination, uint256 amount) external override {
