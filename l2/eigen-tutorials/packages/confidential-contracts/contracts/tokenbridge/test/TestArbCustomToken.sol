e05780cd9d5aeb8e928447dc12c256e4fd2d0d59
eigmax <stephen@ieigen.com>
2021-10-20 15:01:58 +0800

    New CC sample

--- l2/eigen-tutorials/packages/confidential-contracts/contracts/tokenbridge/test/TestArbCustomToken.sol
+++ l2/eigen-tutorials/packages/confidential-contracts/contracts/tokenbridge/test/TestArbCustomToken.sol
@@ -22,29 +22,11 @@ import "../arbitrum/IArbToken.sol";
 import "../libraries/aeERC20.sol";
 import "../arbitrum/ArbTokenBridge.sol";
 
-import "../../arbos/builtin/ArbSys.sol";
-import "./RLPEncode.sol";
-import "./EigenCallHelper.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-
 contract TestArbCustomToken is aeERC20, IArbToken {
-    using EigenCallHelper for *;
-    using Strings for uint256;
     ArbTokenBridge public bridge;
     address public override l1Address;
 
-    // Mapping from token ID to approved address
-    mapping(address => bytes) private _cipher_balances;
-
-    /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
-    event TransferCipher(address indexed from, address indexed to, bytes value);
-
-    modifier onlyBridge() {
+    modifier onlyBridge {
         require(msg.sender == address(bridge), "ONLY_BRIDGE");
         _;
     }
@@ -57,147 +39,15 @@ contract TestArbCustomToken is aeERC20, IArbToken {
 
     function someWackyCustomStuff() public {}
 
-    function cipherBalanceOf(address account) public view virtual returns (bytes memory) {
-        return _cipher_balances[account];
-    }
-
     function bridgeMint(address account, uint256 amount) external override onlyBridge {
         _mint(account, amount);
-
-        bytes memory cipher_base64 = encrypt(amount);
-        _cipher_balances[account] = EigenCallHelper.copy_bytes(cipher_base64);
     }
 
     function bridgeBurn(address account, uint256 amount) external override onlyBridge {
         _burn(account, amount);
-
-        bytes memory cipher_base64_balance = _cipher_balances[account];
-        bytes memory cipher_base64 = EigenCallHelper.subCipherPlain(cipher_base64_balance, amount);
-        _cipher_balances[account] = EigenCallHelper.copy_bytes(cipher_base64);
     }
 
     function withdraw(address destination, uint256 amount) external override {
         bridge.withdraw(l1Address, msg.sender, destination, amount);
-    }
-
-    function cipherTransfer(address recipient, bytes memory cipher_amount)
-        public
-        virtual
-        returns (bool)
-    {
-        require(_msgSender() != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-
-        bytes[] memory list;
-        list = new bytes[](4);
-
-        bytes memory sender_cipher_base64_balance = _cipher_balances[_msgSender()];
-        bytes memory sender_cipher_base64 = EigenCallHelper.subCipherCipher(
-            sender_cipher_base64_balance,
-            cipher_amount
-        );
-        _cipher_balances[_msgSender()] = EigenCallHelper.copy_bytes(sender_cipher_base64);
-
-        bytes memory recipient_cipher_base64_balance = _cipher_balances[recipient];
-        bytes memory recipient_cipher_base64 = EigenCallHelper.addCipherCipher(
-            recipient_cipher_base64_balance,
-            cipher_amount
-        );
-        _cipher_balances[recipient] = EigenCallHelper.copy_bytes(recipient_cipher_base64);
-        emit TransferCipher(_msgSender(), recipient, cipher_amount);
-        return true;
-    }
-
-    function cipherTransferFrom(
-        address sender,
-        address recipient,
-        bytes memory cipher_amount
-    ) public virtual returns (bool) {
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-
-        bytes[] memory list;
-        list = new bytes[](4);
-
-        bytes memory sender_cipher_base64_balance = _cipher_balances[sender];
-        bytes memory sender_cipher_base64 = EigenCallHelper.subCipherCipher(sender_cipher_base64_balance, cipher_amount);
-        _cipher_balances[sender] = EigenCallHelper.copy_bytes(sender_cipher_base64);
-
-        bytes memory recipient_cipher_base64_balance = _cipher_balances[recipient];
-        bytes memory recipient_cipher_base64 = EigenCallHelper.addCipherCipher(
-            recipient_cipher_base64_balance,
-            cipher_amount
-        );
-        _cipher_balances[recipient] = EigenCallHelper.copy_bytes(recipient_cipher_base64);
-        emit TransferCipher(sender, recipient, bytes(cipher_amount));
-
-        return true;
-    }
-
-    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
-        _transfer(_msgSender(), recipient, amount);
-
-        require(_msgSender() != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-
-        bytes[] memory list;
-        list = new bytes[](4);
-
-        bytes memory sender_cipher_base64_balance = _cipher_balances[_msgSender()];
-        bytes memory sender_cipher_base64 = EigenCallHelper.subCipherPlain(sender_cipher_base64_balance, amount);
-        _cipher_balances[_msgSender()] = EigenCallHelper.copy_bytes(sender_cipher_base64);
-
-        bytes memory recipient_cipher_base64_balance = _cipher_balances[recipient];
-        bytes memory recipient_cipher_base64 = EigenCallHelper.addCipherPlain(
-            recipient_cipher_base64_balance,
-            amount
-        );
-        _cipher_balances[recipient] = EigenCallHelper.copy_bytes(recipient_cipher_base64);
-        emit TransferCipher(_msgSender(), recipient, bytes(amount.toString()));
-        return true;
-    }
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) public virtual override returns (bool) {
-        _transfer(sender, recipient, amount);
-
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-
-        bytes[] memory list;
-        list = new bytes[](4);
-
-        bytes memory sender_cipher_base64_balance = _cipher_balances[sender];
-        bytes memory sender_cipher_base64 = EigenCallHelper.subCipherPlain(sender_cipher_base64_balance, amount);
-        _cipher_balances[sender] = EigenCallHelper.copy_bytes(sender_cipher_base64);
-
-        bytes memory recipient_cipher_base64_balance = _cipher_balances[recipient];
-        bytes memory recipient_cipher_base64 = EigenCallHelper.addCipherPlain(
-            recipient_cipher_base64_balance,
-            amount
-        );
-        _cipher_balances[recipient] = EigenCallHelper.copy_bytes(recipient_cipher_base64);
-        emit TransferCipher(sender, recipient, bytes(amount.toString()));
-
-        return true;
-    }
-
-    function cipherBalanceOf(address account, bytes memory secret) public view returns (bytes memory) {
-        bytes memory balance = _cipher_balances[account];
-        // re-encrypt by user's secret
-        return EigenCallHelper.re_encrypt(balance, secret);
-    }
-
-    // for DEBUG
-    function encrypt(uint256 plain) public pure returns (bytes memory) {
-        return EigenCallHelper.execute("encrypt1", bytes(plain.toString()), "", "");
-    }
-
-    // for DEBUG
-    function decrypt(bytes memory cipher) public pure returns (bytes memory) {
-        return EigenCallHelper.execute("decrypt1", cipher, "", "");
     }
 }
--- l2/eigen-tutorials/packages/demo-eigencall/scripts/exec.js
+++ l2/eigen-tutorials/packages/demo-eigencall/scripts/exec.js
@@ -14,294 +14,294 @@ require('dotenv').config()
 requireEnvVariables(['DEVNET_PRIVKEY', 'L2RPC', 'PKCS'])
 
 function compose_decrypt(cipher) {
-    return RLP.encode(["decrypt1", Base64.fromUint8Array(cipher), "", ""])
+  return RLP.encode(["decrypt1", Base64.fromUint8Array(cipher), "", ""])
 }
 
 function compose_encrypt(num) {
-    return RLP.encode(["encrypt1", num.toString(), "", ""])
+  return RLP.encode(["encrypt1", num.toString(), "", ""])
 }
 
 function compose_add_cipher_cipher(cipher1, cipher2) {
-    return RLP.encode(["add_cipher_cipher2", Base64.fromUint8Array(cipher1), Base64.fromUint8Array(cipher2), ""])
+  return RLP.encode(["add_cipher_cipher2", Base64.fromUint8Array(cipher1), Base64.fromUint8Array(cipher2), ""])
 }
 
 function compose_add_cipher_plain(cipher, plain) {
-    return RLP.encode(["add_cipher_plain2", Base64.fromUint8Array(cipher), plain.toString(), ""])
+  return RLP.encode(["add_cipher_plain2", Base64.fromUint8Array(cipher), plain.toString(), ""])
 }
 
 function compose_sub_cipher_cipher(cipher1, cipher2) {
-    return RLP.encode(["sub_cipher_cipher2", Base64.fromUint8Array(cipher1), Base64.fromUint8Array(cipher2), ""])
+  return RLP.encode(["sub_cipher_cipher2", Base64.fromUint8Array(cipher1), Base64.fromUint8Array(cipher2), ""])
 }
 
 function compose_sub_cipher_plain(cipher, plain) {
-    return RLP.encode(["sub_cipher_plain2", Base64.fromUint8Array(cipher), plain.toString(), ""])
+  return RLP.encode(["sub_cipher_plain2", Base64.fromUint8Array(cipher), plain.toString(), ""])
 }
 
 const encrypt = async (contract, num) => {
-    var encrypt_operator_encoding_string = compose_encrypt(num)
-    var tx = await contract.call_eigenCall(encrypt_operator_encoding_string, {
-        gasPrice: 0,
-        gasLimit: 250000
-    })
+  var encrypt_operator_encoding_string = compose_encrypt(num)
+  var tx = await contract.call_eigenCall(encrypt_operator_encoding_string, {
+    gasPrice: 0,
+    gasLimit: 250000
+  })
 
-    var receipt = await tx.wait()
-    var event = receipt.events.pop()
+  var receipt = await tx.wait()
+  var event = receipt.events.pop()
 
-    rlp_encoded_return_value = event.args.returnValue;
+  rlp_encoded_return_value = event.args.returnValue;
 
-    expect(rlp_encoded_return_value).not.equal(RLP.encode(""))
+  expect(rlp_encoded_return_value).not.equal(RLP.encode(""))
 
-    var cipher_base64 = RLP.decode(rlp_encoded_return_value).toString()
-    var cipher = Base64.toUint8Array(cipher_base64)
-    return cipher
+  var cipher_base64 = RLP.decode(rlp_encoded_return_value).toString()
+  var cipher = Base64.toUint8Array(cipher_base64)
+  return cipher
 }
 
 const decrypt = async (contract, cipher) => {
-    var decrypt_operator_encoding_string = compose_decrypt(cipher)
+  var decrypt_operator_encoding_string = compose_decrypt(cipher)
 
-    var tx = await contract.call_eigenCall(decrypt_operator_encoding_string, {
-        gasPrice: 0,
-        gasLimit: 250000
-    })
+  var tx = await contract.call_eigenCall(decrypt_operator_encoding_string, {
+    gasPrice: 0,
+    gasLimit: 250000
+  })
 
-    var receipt = await tx.wait()
-    var event = receipt.events.pop()
+  var receipt = await tx.wait()
+  var event = receipt.events.pop()
 
-    rlp_encoded_return_value = event.args.returnValue;
+  rlp_encoded_return_value = event.args.returnValue;
 
-    expect(rlp_encoded_return_value).not.equal(RLP.encode(""))
+  expect(rlp_encoded_return_value).not.equal(RLP.encode(""))
 
-    var plain = RLP.decode(rlp_encoded_return_value).toString()
+  var plain = RLP.decode(rlp_encoded_return_value).toString()
 
-    return parseInt(plain)
+  return parseInt(plain)
 }
 
 const add_cipher_cipher = async (contract, cipher1, cipher2) => {
-    var add_cipher_cipher_operator_encoding_string = compose_add_cipher_cipher(cipher1, cipher2)
-    var tx = await contract.call_eigenCall(add_cipher_cipher_operator_encoding_string, {
-        gasPrice: 0,
-        gasLimit: 250000
-    })
+  var add_cipher_cipher_operator_encoding_string = compose_add_cipher_cipher(cipher1, cipher2)
+  var tx = await contract.call_eigenCall(add_cipher_cipher_operator_encoding_string, {
+    gasPrice: 0,
+    gasLimit: 250000
+  })
 
-    var receipt = await tx.wait()
-    var event = receipt.events.pop()
+  var receipt = await tx.wait()
+  var event = receipt.events.pop()
 
-    rlp_encoded_return_value = event.args.returnValue;
+  rlp_encoded_return_value = event.args.returnValue;
 
-    expect(rlp_encoded_return_value).not.equal(RLP.encode(""))
+  expect(rlp_encoded_return_value).not.equal(RLP.encode(""))
 
-    var cipher_base64 = RLP.decode(rlp_encoded_return_value).toString()
-    var cipher = Base64.toUint8Array(cipher_base64)
-    return cipher
+  var cipher_base64 = RLP.decode(rlp_encoded_return_value).toString()
+  var cipher = Base64.toUint8Array(cipher_base64)
+  return cipher
 }
 
 const add_cipher_plain = async (contract, cipher, plain) => {
-    var add_cipher_plain_operator_encoding_string = compose_add_cipher_plain(cipher, plain)
-    var tx = await contract.call_eigenCall(add_cipher_plain_operator_encoding_string, {
-        gasPrice: 0,
-        gasLimit: 250000
-    })
+  var add_cipher_plain_operator_encoding_string = compose_add_cipher_plain(cipher, plain)
+  var tx = await contract.call_eigenCall(add_cipher_plain_operator_encoding_string, {
+    gasPrice: 0,
+    gasLimit: 250000
+  })
 
-    var receipt = await tx.wait()
-    var event = receipt.events.pop()
+  var receipt = await tx.wait()
+  var event = receipt.events.pop()
 
-    rlp_encoded_return_value = event.args.returnValue;
+  rlp_encoded_return_value = event.args.returnValue;
 
-    expect(rlp_encoded_return_value).not.equal(RLP.encode(""))
+  expect(rlp_encoded_return_value).not.equal(RLP.encode(""))
 
-    var cipher_base64 = RLP.decode(rlp_encoded_return_value).toString()
-    var cipher = Base64.toUint8Array(cipher_base64)
-    return cipher
+  var cipher_base64 = RLP.decode(rlp_encoded_return_value).toString()
+  var cipher = Base64.toUint8Array(cipher_base64)
+  return cipher
 }
 
 const sub_cipher_cipher = async (contract, cipher1, cipher2) => {
-    var sub_cipher_cipher_operator_encoding_string = compose_sub_cipher_cipher(cipher1, cipher2)
-    var tx = await contract.call_eigenCall(sub_cipher_cipher_operator_encoding_string, {
-        gasPrice: 0,
-        gasLimit: 250000
-    })
+  var sub_cipher_cipher_operator_encoding_string = compose_sub_cipher_cipher(cipher1, cipher2)
+  var tx = await contract.call_eigenCall(sub_cipher_cipher_operator_encoding_string, {
+    gasPrice: 0,
+    gasLimit: 250000
+  })
 
-    var receipt = await tx.wait()
-    var event = receipt.events.pop()
+  var receipt = await tx.wait()
+  var event = receipt.events.pop()
 
-    rlp_encoded_return_value = event.args.returnValue;
+  rlp_encoded_return_value = event.args.returnValue;
 
-    expect(rlp_encoded_return_value).not.equal(RLP.encode(""))
+  expect(rlp_encoded_return_value).not.equal(RLP.encode(""))
 
-    var cipher_base64 = RLP.decode(rlp_encoded_return_value).toString()
-    var cipher = Base64.toUint8Array(cipher_base64)
-    return cipher
+  var cipher_base64 = RLP.decode(rlp_encoded_return_value).toString()
+  var cipher = Base64.toUint8Array(cipher_base64)
+  return cipher
 }
 
 const sub_cipher_plain = async (contract, cipher, plain) => {
-    var sub_cipher_plain_operator_encoding_string = compose_sub_cipher_plain(cipher, plain)
-    var tx = await contract.call_eigenCall(sub_cipher_plain_operator_encoding_string, {
-        gasPrice: 0,
-        gasLimit: 250000
-    })
+  var sub_cipher_plain_operator_encoding_string = compose_sub_cipher_plain(cipher, plain)
+  var tx = await contract.call_eigenCall(sub_cipher_plain_operator_encoding_string, {
+    gasPrice: 0,
+    gasLimit: 250000
+  })
 
-    var receipt = await tx.wait()
-    var event = receipt.events.pop()
+  var receipt = await tx.wait()
+  var event = receipt.events.pop()
 
-    rlp_encoded_return_value = event.args.returnValue;
+  rlp_encoded_return_value = event.args.returnValue;
 
-    expect(rlp_encoded_return_value).not.equal(RLP.encode(""))
+  expect(rlp_encoded_return_value).not.equal(RLP.encode(""))
 
-    var cipher_base64 = RLP.decode(rlp_encoded_return_value).toString()
-    var cipher = Base64.toUint8Array(cipher_base64)
-    return cipher
+  var cipher_base64 = RLP.decode(rlp_encoded_return_value).toString()
+  var cipher = Base64.toUint8Array(cipher_base64)
+  return cipher
 }
 
 function ecies_encrypt(public_key, num) {
-    // default option
-    const options = {
-        hashName: 'sha512',
-        hashLength: 64,
-        macName: 'sha256',
-        macLength: 32,
-        curveName: 'prime256v1',
-        symmetricCypherName: 'aes-256-gcm',
-        keyFormat: 'uncompressed',
-        s1: null, // optional shared information1
-        s2: null // optional shared information2
-    }
+  // default option
+  const options = {
+    hashName: 'sha512',
+    hashLength: 64,
+    macName: 'sha256',
+    macLength: 32,
+    curveName: 'prime256v1',
+    symmetricCypherName: 'aes-256-gcm',
+    keyFormat: 'uncompressed',
+    s1: null, // optional shared information1
+    s2: null // optional shared information2
+  }
 
-    const cipher = ecies.encrypt(public_key, (new Uint64BE(num)).toBuffer(), options);
+  const cipher = ecies.encrypt(public_key, (new Uint64BE(num)).toBuffer(), options);
 
-    return cipher
+  return cipher
 }
 
 const main = async () => {
-    await eigenLog('Simple eigenCall demo')
+  await eigenLog('Simple eigenCall demo')
 
-    const res = await fetch(process.env['PKCS'] + '/store?digest=1', {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' }
-    })
-        .then(response => response.json())
+  const res = await fetch(process.env['PKCS'] + '/store?digest=1', {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then(response => response.json())
 
-    expect(res.errno).to.equal(0)
+  expect(res.errno).to.equal(0)
 
-    const public_key = res.data.public_key
+  const public_key = res.data.public_key
 
-    console.log("Public key get from pkcs: ", public_key)
+  console.log("Public key get from pkcs: ", public_key)
 
-    expect(public_key).not.equal("")
+  expect(public_key).not.equal("")
 
-    let keyPair = ec.keyFromPublic(public_key, "hex");
-    let publicKey = keyPair.getPublic();
+  let keyPair = ec.keyFromPublic(public_key, "hex");
+  let publicKey = keyPair.getPublic();
 
-    console.log("----------------------------------------------------")
+  console.log("----------------------------------------------------")
 
-    const l2Wallet = (await hre.ethers.getSigners())[0]
-    console.log('Your wallet address:', l2Wallet.address)
+  const l2Wallet = (await hre.ethers.getSigners())[0]
+  console.log('Your wallet address:', l2Wallet.address)
 
-    const L2EigencallDemo = await (
-        await ethers.getContractFactory('EigencallDemo')
-    ).connect(l2Wallet)
-    console.log('Deploying EigencallDemo contract to L2')
-    const l2eigencalldemo = await L2EigencallDemo.deploy({ gasLimit: 250000 })
-    await l2eigencalldemo.deployed()
-    console.log(`EigencallDemo contract is deployed to ${l2eigencalldemo.address}`)
+  const L2EigencallDemo = await (
+    await ethers.getContractFactory('EigencallDemo')
+  ).connect(l2Wallet)
+  console.log('Deploying EigencallDemo contract to L2')
+  const l2eigencalldemo = await L2EigencallDemo.deploy({ gasLimit: 250000 })
+  await l2eigencalldemo.deployed()
+  console.log(`EigencallDemo contract is deployed to ${l2eigencalldemo.address}`)
 
-    ////////////////////////////////////////////////////////////////////////////////////
-    // 'encrypt' test
-    var num = 123
-    var cipher = ecies_encrypt(publicKey, num)
+  ////////////////////////////////////////////////////////////////////////////////////
+  // 'encrypt' test
+  var num = 123
+  var cipher = ecies_encrypt(publicKey, num)
 
-    // 'decrypt' test
-    console.log("Going to 'decrypt' ", cipher)
-    var decrypt_number = await decrypt(l2eigencalldemo, cipher)
-    console.log("Success!")
+  // 'decrypt' test
+  console.log("Going to 'decrypt' ", cipher)
+  var decrypt_number = await decrypt(l2eigencalldemo, cipher)
+  console.log("Success!")
 
-    expect(decrypt_number).to.equal(num)
-    console.log(`${num} -> encrypt -> decrypt is still ${num}`)
-    ////////////////////////////////////////////////////////////////////////////////////
+  expect(decrypt_number).to.equal(num)
+  console.log(`${num} -> encrypt -> decrypt is still ${num}`)
+  ////////////////////////////////////////////////////////////////////////////////////
 
-    ////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////
     // Self 'encrypt' and then 'decrypt' test
     var num = 123
-    console.log("Going to 'encrypt' ", num)
-    var cipher = await encrypt(l2eigencalldemo, num)
-    console.log("Success!")
+  console.log("Going to 'encrypt' ", num)
+  var cipher = await encrypt(l2eigencalldemo, num)
+  console.log("Success!")
 
-    // 'decrypt' test
-    console.log("Going to 'decrypt' ", cipher)
-    var decrypt_number = await decrypt(l2eigencalldemo, cipher)
-    console.log("Success!")
+  // 'decrypt' test
+  console.log("Going to 'decrypt' ", cipher)
+  var decrypt_number = await decrypt(l2eigencalldemo, cipher)
+  console.log("Success!")
 
-    expect(decrypt_number).to.equal(num)
-    console.log(`${num} -> encrypt -> decrypt is still ${num}`)
-    ////////////////////////////////////////////////////////////////////////////////////
+  expect(decrypt_number).to.equal(num)
+  console.log(`${num} -> encrypt -> decrypt is still ${num}`)
+  ////////////////////////////////////////////////////////////////////////////////////
 
-    ////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////
     // 'add_cipher_cipher' test
     var num1 = 100
-    var num2 = 1
+  var num2 = 1
 
-    console.log(`Going to 'add_cipher_cipher' ${num1}, ${num2}`)
-    var cipher1 = ecies_encrypt(publicKey, num1)
-    var cipher2 = ecies_encrypt(publicKey, num2)
+  console.log(`Going to 'add_cipher_cipher' ${num1}, ${num2}`)
+  var cipher1 = ecies_encrypt(publicKey, num1)
+  var cipher2 = ecies_encrypt(publicKey, num2)
 
-    var result_cipher = await add_cipher_cipher(l2eigencalldemo, cipher1, cipher2)
-    var result_number = await decrypt(l2eigencalldemo, result_cipher)
+  var result_cipher = await add_cipher_cipher(l2eigencalldemo, cipher1, cipher2)
+  var result_number = await decrypt(l2eigencalldemo, result_cipher)
 
-    expect(result_number).to.equal(num1 + num2)
-    console.log(`add_cipher_cipher ${num1} ${num2} is ${num1 + num2}`)
-    ////////////////////////////////////////////////////////////////////////////////////
+  expect(result_number).to.equal(num1 + num2)
+  console.log(`add_cipher_cipher ${num1} ${num2} is ${num1 + num2}`)
+  ////////////////////////////////////////////////////////////////////////////////////
 
-    ////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////
     // 'add_cipher_plain' test
     var num1 = 100
-    var num2 = 1
+  var num2 = 1
 
-    console.log(`Going to 'add_cipher_plain' ${num1}, ${num2}`)
-    var cipher1 = ecies_encrypt(publicKey, num1)
+  console.log(`Going to 'add_cipher_plain' ${num1}, ${num2}`)
+  var cipher1 = ecies_encrypt(publicKey, num1)
 
-    var result_cipher = await add_cipher_plain(l2eigencalldemo, cipher1, num2)
-    var result_number = await decrypt(l2eigencalldemo, result_cipher)
+  var result_cipher = await add_cipher_plain(l2eigencalldemo, cipher1, num2)
+  var result_number = await decrypt(l2eigencalldemo, result_cipher)
 
-    expect(result_number).to.equal(num1 + num2)
-    console.log(`add_cipher_plain ${num1} ${num2} is ${num1 + num2}`)
-    ////////////////////////////////////////////////////////////////////////////////////
+  expect(result_number).to.equal(num1 + num2)
+  console.log(`add_cipher_plain ${num1} ${num2} is ${num1 + num2}`)
+  ////////////////////////////////////////////////////////////////////////////////////
 
-    ////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////
     // 'sub_cipher_cipher' test
     var num1 = 100
-    var num2 = 1
+  var num2 = 1
 
-    console.log(`Going to 'sub_cipher_cipher' ${num1}, ${num2}`)
-    var cipher1 = ecies_encrypt(publicKey, num1)
-    var cipher2 = ecies_encrypt(publicKey, num2)
+  console.log(`Going to 'sub_cipher_cipher' ${num1}, ${num2}`)
+  var cipher1 = ecies_encrypt(publicKey, num1)
+  var cipher2 = ecies_encrypt(publicKey, num2)
 
-    var result_cipher = await sub_cipher_cipher(l2eigencalldemo, cipher1, cipher2)
-    var result_number = await decrypt(l2eigencalldemo, result_cipher)
+  var result_cipher = await sub_cipher_cipher(l2eigencalldemo, cipher1, cipher2)
+  var result_number = await decrypt(l2eigencalldemo, result_cipher)
 
-    expect(result_number).to.equal(num1 - num2)
-    console.log(`sub_cipher_cipher ${num1} ${num2} is ${num1 - num2}`)
-    ////////////////////////////////////////////////////////////////////////////////////
+  expect(result_number).to.equal(num1 - num2)
+  console.log(`sub_cipher_cipher ${num1} ${num2} is ${num1 - num2}`)
+  ////////////////////////////////////////////////////////////////////////////////////
 
-    ////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////
     // 'sub_cipher_plain' test
     var num1 = 100
-    var num2 = 1
+  var num2 = 1
 
-    console.log(`Going to 'sub_cipher_plain' ${num1}, ${num2}`)
-    var cipher1 = ecies_encrypt(publicKey, num1)
+  console.log(`Going to 'sub_cipher_plain' ${num1}, ${num2}`)
+  var cipher1 = ecies_encrypt(publicKey, num1)
 
-    var result_cipher = await sub_cipher_plain(l2eigencalldemo, cipher1, num2)
-    var result_number = await decrypt(l2eigencalldemo, result_cipher)
+  var result_cipher = await sub_cipher_plain(l2eigencalldemo, cipher1, num2)
+  var result_number = await decrypt(l2eigencalldemo, result_cipher)
 
-    expect(result_number).to.equal(num1 - num2)
-    console.log(`sub_cipher_plain ${num1} ${num2} is ${num1 - num2}`)
-    ////////////////////////////////////////////////////////////////////////////////////
+  expect(result_number).to.equal(num1 - num2)
+  console.log(`sub_cipher_plain ${num1} ${num2} is ${num1 - num2}`)
+  ////////////////////////////////////////////////////////////////////////////////////
 
-    console.log("All test success!")
+  console.log("All test success!")
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch(error => {
-        console.error(error)
-        process.exit(1)
-    })
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
