8a670c828c1a4e25e3ac8e05bdebd8e535de652d
Weber❤️H2O2 <82763013+Weber-H2O2@users.noreply.github.com>
2021-10-24 21:48:41 +0800

    Use cors to allow all origins

--- l2/eigen_service/src/app.ts
+++ l2/eigen_service/src/app.ts
@@ -9,6 +9,8 @@ import { Op } from "sequelize";
 
 import { JWT_SECRET } from "./login/config";
 
+import cors from "cors";
+
 import * as userdb from "./pid/pid";
 import * as friend_list from "./database_friend_relationship";
 
@@ -30,18 +32,28 @@ app.use({
 })
 
 */
-app.use((req, res, next) => {
-  if (req.path !== "/" && !req.path.includes(".")) {
-    res.set({
-      "Access-Control-Allow-Credentials": true, //允许后端发送cookie
-      "Access-Control-Allow-Origin": req.headers.origin || "*", //任意域名都可以访问,或者基于我请求头里面的域
-      "Access-Control-Allow-Headers": "X-Requested-With,Content-Type", //设置请求头格式和类型
-      "Access-Control-Allow-Methods": "PUT,POST,GET,DELETE,OPTIONS", //允许支持的请求方式
-      "Content-Type": "application/json; charset=utf-8", //默认与允许的文本格式json和编码格式
-    });
-  }
-  req.method === "OPTIONS" ? res.status(204).end() : next();
-});
+// app.use((req, res, next) => {
+//   if (req.path !== "/" && !req.path.includes(".")) {
+//     res.set({
+//       "Access-Control-Allow-Credentials": true, //允许后端发送cookie
+//       "Access-Control-Allow-Origin": req.headers.origin || "*", //任意域名都可以访问,或者基于我请求头里面的域
+//       "Access-Control-Allow-Headers": "X-Requested-With,Content-Type", //设置请求头格式和类型
+//       "Access-Control-Allow-Methods": "PUT,POST,GET,DELETE,OPTIONS", //允许支持的请求方式
+//       "Content-Type": "application/json; charset=utf-8", //默认与允许的文本格式json和编码格式
+//     });
+//   }
+//   req.method === "OPTIONS" ? res.status(204).end() : next();
+// });
+
+app.use(cors);
+app.use(express.json());
+const allowedOrigins = ["*"];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
 
 // query key
 app.get("/stores", async function (req, res) {
