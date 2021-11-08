c129e8ad8a0fdaf1d966c61e0f864c1f85d07b15
Weber-H2O2 <82763013+Weber-H2O2@users.noreply.github.com>
2021-11-08 21:31:07 +0800

    EigenCallLibrary now can be called on cc.ts

--- l2/eigen-tutorials/packages/confidential-contracts/contracts/tokenbridge/test/TestCCCustomToken.sol
+++ l2/eigen-tutorials/packages/confidential-contracts/contracts/tokenbridge/test/TestCCCustomToken.sol
@@ -197,4 +197,69 @@ contract TestCCCustomToken is aeERC20, IArbToken {
         emit TransferCipher(sender, recipient, bytes(amount.toString()));
         return true;
     }
+
+    function demo_encrypt(uint256 plain) public pure returns (bytes memory) {
+        bytes memory output = EigenCallLibrary.encrypt(plain);
+        return output;
+    }
+
+    function demo_decrypt(bytes memory cipher) public pure returns (bytes memory) {
+        bytes memory output = EigenCallLibrary.decrypt(cipher);
+
+        return output;
+    }
+
+    function demo_addCipherCipher(bytes memory cipher1, bytes memory cipher2)
+        public
+        pure
+        returns (bytes memory)
+    {
+        bytes memory output = EigenCallLibrary.addCipherCipher(cipher1, cipher2);
+        return output;
+    }
+
+    function demo_addCipherPlain(bytes memory cipher, uint256 plain)
+        public
+        pure
+        returns (bytes memory)
+    {
+        bytes memory output = EigenCallLibrary.addCipherPlain(cipher, plain);
+        return output;
+    }
+
+    function demo_subCipherCipher(bytes memory cipher1, bytes memory cipher2)
+        public
+        pure
+        returns (bytes memory)
+    {
+        bytes memory output = EigenCallLibrary.subCipherCipher(cipher1, cipher2);
+        return output;
+    }
+
+    function demo_subCipherPlain(bytes memory cipher, uint256 plain)
+        public
+        pure
+        returns (bytes memory)
+    {
+        bytes memory output = EigenCallLibrary.subCipherPlain(cipher, plain);
+        return output;
+    }
+
+    function demo_compareCipherCipher(bytes memory cipher1, bytes memory cipher2)
+        public
+        pure
+        returns (int256)
+    {
+        int256 result = EigenCallLibrary.compareCipherCipher(cipher1, cipher2);
+        return result;
+    }
+
+    function demo_compareCipherPlain(bytes memory cipher, uint256 plain)
+        public
+        pure
+        returns (int256)
+    {
+        int256 result = EigenCallLibrary.compareCipherPlain(cipher, plain);
+        return result;
+    }
 }
